98dcd52346245a6f793d129403b0a93fec583905
Mrflaxe <dmitriyclon@gmail.com>
2020-09-16 20:56:57 +0300

    update 1.1.0

--- src/main/java/ru/mrflaxe/randomdrop/RandomDrop.java
+++ src/main/java/ru/mrflaxe/randomdrop/RandomDrop.java
@@ -4,10 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import ru.mrflaxe.randomdrop.listeners.PlayerDeathListener;
 import ru.soknight.lib.configuration.Configuration;
+import ru.soknight.lib.configuration.Messages;
 
 public class RandomDrop extends JavaPlugin{
 	
 	private Configuration config;
+	private Messages messages;
 	
 	@Override
 	public void onEnable() {
@@ -19,9 +21,13 @@ public class RandomDrop extends JavaPlugin{
 	private void initConfigs() {
 		this.config = new Configuration(this, "config.yml");
 		config.refresh();
+		
+		this.messages = new Messages(this, "messages.yml");
+		messages.refresh();
+		
 	}
 	
 	private void registerEvents() {
-		new PlayerDeathListener(config).register(this);
+		new PlayerDeathListener(config, messages, this).register(this);
 	}
 }
--- src/main/java/ru/mrflaxe/randomdrop/listeners/PlayerDeathListener.java
+++ src/main/java/ru/mrflaxe/randomdrop/listeners/PlayerDeathListener.java
@@ -4,42 +4,59 @@ import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import ru.soknight.lib.configuration.Configuration;
+import ru.soknight.lib.configuration.Messages;
 
 public class PlayerDeathListener implements Listener{
 	
 	private final Configuration config;
+	private final Messages messages;
 	
-	public PlayerDeathListener(Configuration config) {
+	private final JavaPlugin plugin;
+	
+	public PlayerDeathListener(Configuration config,Messages messages, JavaPlugin plugin) {
 		this.config = config;
+		this.messages = messages;
+		this.plugin = plugin;
 	}
 	
-	@EventHandler
+	@EventHandler (priority = EventPriority.HIGH)
 	public void PlayerDeathevent(PlayerDeathEvent e) {
 		Player p = e.getEntity();
 		PlayerInventory inv = p.getInventory();
 		List<ItemStack> drops = e.getDrops();
 		
-		e.setKeepInventory(true);
-		List<ItemStack> loot = drops.parallelStream().filter(i -> isFall()).collect(Collectors.toList());
+		List<String> worlds = config.getList("worlds");
+		if(!worlds.contains(p.getWorld().getName())) return;
+		
+		List<ItemStack> drop = drops.parallelStream().filter(i -> isFall()).collect(Collectors.toList());
+		List<ItemStack> loot = drops.parallelStream().filter(i -> !drop.contains(i)).collect(Collectors.toList());
 		drops.clear();
 
+		Inventory storage = Bukkit.createInventory(null, 54);
+		System.out.println(loot.size());
+		loot.stream().forEach(i -> storage.addItem(i));
+		
 		Location loc = p.getLocation();
-		System.out.println("loot.size = " + loot.size());
-		loot.stream().peek(i -> loc.getWorld().dropItemNaturally(loc, i))
-		.forEach(i -> {
-			int f = inv.first(i);
-			inv.clear(f);
-		});
+		drop.stream().peek(i -> loc.getWorld().dropItemNaturally(loc, i))
+				.forEach(i -> inv.clear(inv.first(i)));
+		
+		Bukkit.getScheduler().runTaskLater(plugin, task -> {
+			p.openInventory(storage);
+			messages.getAndSend(p, "attention");
+		}, 20);
 	}
 	
 	private boolean isFall() {
